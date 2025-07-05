a5884c97f8b080a3fbd1dcfd045746ea65b8d865
Oweta Jacob Emmy <oweta001@gmail.com>
2025-07-05 11:46:08 +0300

    Update Location.java

--- src/Location.java
+++ src/Location.java
@@ -1,3 +1,23 @@
 public class Location {
-    
+    private int x;
+    private int y;
+
+    public Location(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
 }
